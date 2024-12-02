7c975e74e072fbec02868019d9d1ef637ad01640
Diana <anganangandianarose@gmail.com>
2024-12-02 19:54:14 +0800

    Initial

---
+++ SpaceEnroll/Controllers/.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaceEnroll.Controllers
+{
+    public class CoderController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
